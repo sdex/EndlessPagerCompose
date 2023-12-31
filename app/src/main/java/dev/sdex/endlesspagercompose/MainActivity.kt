cf406802ba7ca569ecce9c24d7b9a07977b2201b
Mysochenko Yuriy <yuriy.mysochenko@gmail.com>
2023-12-31 18:31:26 +0100

    first commit

---
+++ app/src/main/java/dev/sdex/endlesspagercompose/MainActivity.kt
@@ -0,0 +1,55 @@
+package dev.sdex.endlesspagercompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.pager.VerticalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
+import dev.sdex.endlesspagercompose.ui.theme.EndlessPagerComposeTheme
+
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalFoundationApi::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            EndlessPagerComposeTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val pageCount = 6
+                    val pagerState = rememberPagerState(Int.MAX_VALUE / 2 - pageCount / 2) {
+                        Int.MAX_VALUE
+                    }
+                    VerticalPager(
+                        state = pagerState,
+                        modifier = Modifier.fillMaxSize()
+                    ) { page ->
+                        Box(
+                            modifier = Modifier
+                                .fillMaxSize(),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Text(
+                                text = "Page: ${(page % 6)}\n(Real position: $page)",
+                                fontSize = 32.sp,
+                                modifier = Modifier.align(Alignment.Center),
+                                textAlign = TextAlign.Center,
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
